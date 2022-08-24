690a37e8c739cd99377886a5e3ce85c3bc9244d7
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-08-24 15:52:41 +1000

    push: cycle `for`

---
+++ Study/for.js
@@ -0,0 +1,14 @@
+let fibo = [];
+fibo.push(1);
+fibo.push(1);
+fibo.push(2);
+fibo.push(3);
+fibo.push(5);
+fibo.push(8);
+fibo.push(13);
+
+let fibo = [1, 1]
+for (let i = 1; i < 49; ++i) {
+    fibo.push(fibo[i] + fibo[i - 1]);
+}
+console.log(fibo);
