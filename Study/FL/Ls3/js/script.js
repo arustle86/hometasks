ed768e5f23c3410c5a4e214e0cec3e3096913ea8
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-08-24 17:48:57 +1000

    commit: Ls3

---
+++ Study/FL/Ls3/js/script.js
@@ -0,0 +1,45 @@
+`use strict`;
+let age = 23;
+let newAge = 58;
+age = newAge;
+console.log (age);
+
+
+
+let myAge = 18;
+
+function testBlock() {
+    let myAge = 25;
+    console.log (myAge);
+}
+testBlock();
+
+console.log (myAge);
+
+
+
+const userProfile = {
+    name: `alexsushka`,
+    age: 36,
+    message: `hi man!`
+}
+    console.log (userProfile);
+
+    userProfile.name = `dmitry`;
+
+    console.log (userProfile);
+
+let eyeColor = `brown`;
+console.log (eyeColor);
+
+
+
+let user = `Николай`;
+let userName = `Вася`;
+console.log (user);
+
+user = userName;
+console.log (user);
+
+
+
