9d4a82a5e24e1155173eea66d41cc66ea2facc7b
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-08-24 20:26:46 +1000

    commit: Ls5

---
+++ Study/FL/Ls5/Js/script.js
@@ -0,0 +1,61 @@
+// let resultsOne = `Фрилансер ` +  `по ` + `жизни`;
+// console.log(resultsOne);
+
+// let resultsTwo = `Frelance ` + 58;
+// console.log(resultsTwo);
+// console.log(typeof resultsTwo);
+
+
+// let resultsThree = 2 + `2`;
+// console.log(resultsThree);
+// console.log(typeof resultsThree);
+
+
+// let resultsFour = 58 - 27 + ` Frilance`;
+// console.log(resultsFour);
+// console.log(typeof resultsFour);
+
+// let a = 2 + 1;
+// let b = 2;
+// let result = 8 - (a = b + 3)
+// console.log(`Result in skobki ` + a);
+// console.log(`Result `+ result)
+
+
+// let users = 5;
+// console.log(users);
+// users += 1 * 2;
+// console.log(users); 
+// console.log(typeof users);
+
+
+// let userCounter = 0;
+// console.log(userCounter++);
+// console.log(userCounter);
+
+
+// let userCounter = 0;
+// let newUsers = 2 * ++userCounter;
+// console.log(newUsers);
+
+
+// let userCounter = 0;
+// ++userCounter;
+// let newUsers = 2 * userCounter;
+// console.log(newUsers);
+
+
+// let userCounter = (8 + 2, 3 - 6);
+// console.log (userCounter);
+
+
+// let userName = ``;
+// let userNicName = `Frilance`;
+// let user = userName || userNicName || `Not Corrects`;
+// console.log(user);
+
+
+// let admins = 0;
+// let users = 5;
+// admins > users || users++;
+// console.log(users)
