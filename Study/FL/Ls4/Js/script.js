e63eb757a56664443c630d939bfa9ee652b666c7
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-08-24 19:04:45 +1000

    commit: Ls4

---
+++ Study/FL/Ls4/Js/script.js
@@ -0,0 +1,154 @@
+// let userName;
+// console.log (typeof userName);
+
+
+// userName = `Max`;
+// console.log (typeof userName);
+
+
+// userName = 38;
+// console.log (typeof userName);
+
+
+// if (userName === undefined) {
+//     console.log (`Переменная определена`)
+// } else {
+//     console.log (`Переменная не определена`)
+// }
+
+
+// if (typeof userName === `undefined`) {
+//     console.log (`Переменная определена`)
+// } else {
+//     console.log (`Переменная не определена`)
+// }
+
+
+// let userName = null;
+// console.log (userName);
+
+
+// let block = document.querySelector(`.block`);
+// console.log (block);
+// console.log (typeof block);
+
+
+// let willYouMarryMe = false;
+// if (willYouMarryMe) {
+//     console.log (`=)`)
+// } else {
+//     console.log (`=(`)
+// }
+
+
+// let trueOfFalse = 47 > 25;
+// console.log (trueOfFalse);
+
+
+// let userAge = 38;
+// let userHeight = 1.83;
+// console.log (userAge);
+// console.log (typeof userAge);
+// console.log (userHeight);
+// console.log (typeof userHeight);
+
+
+// let getInfinity = 57 / 0;
+// console.log (getInfinity);
+// console.log (typeof getInfinity);
+
+
+// let getNun = 75 / `JS`;
+// console.log (getNun);
+// console.log (typeof getNun);
+
+
+// const bigInteger = 1234567890123456789n;
+// console.log (bigInteger);
+// console.log (typeof bigInteger);
+
+
+// let userAge = 36;
+// let userAgeInfo = `Возраст: ${userAge}`
+// console.log (userAgeInfo);
+
+
+// let userInfo = {
+//     age: 36,
+//     hight: 1.82,
+//     name: `Tanya`
+// }
+// console.log (userInfo);
+// console.log (typeof userInfo);
+
+
+// let id = Symbol (`id`);
+// console.log (id);
+// console.log (typeof id);
+
+
+
+// let funcVariable = function name(params) {
+
+// }
+// console.log (typeof funcVariable);
+
+
+
+// let userAge = 58;
+// console.log(userAge);
+// console.log(typeof userAge);
+// userAge = String(userAge);
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+// let userTrue = true;
+// console.log(userTrue);
+// console.log(typeof userTrue);
+// userTrue = String(userTrue);
+// console.log(userTrue);
+// console.log(typeof userTrue);
+
+
+// let userAge = `58`;
+// console.log(userAge);
+// console.log(typeof userAge)
+// userAge = Number(userAge);
+// console.log(userAge);
+// console.log(typeof userAge)
+
+
+
+// let userAge = `72` / `36`;
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+// let userAge = 1;
+// console.log(userAge);
+// console.log(typeof userAge);
+// userAge = Boolean(userAge);
+// console.log(userAge);
+// console.log(typeof userAge);
+
+
+
+// let userAge = 36;
+// let userInfo = `Frilanse ${userAge}`;
+// console.log(userInfo);
+
+
+
+// let userHeight = 145 / 0;
+// console.log(userHeight);
+
+
+
+// let userName;
+// console.log(typeof userName);
+
+
+
+let userSize = `57` - `27`;
+console.log(typeof userSize);
