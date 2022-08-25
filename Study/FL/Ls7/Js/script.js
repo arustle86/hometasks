2c468f33e3b5125b3240cb338dad0f2504e14ff7
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-08-25 20:06:15 +1000

    Ls7

---
+++ Study/FL/Ls7/Js/script.js
@@ -0,0 +1,126 @@
+// let num = 0;
+// while (num < 5) {
+//     console.log (num);
+//     num++;
+// }
+
+
+// let num = 5;
+//     while (num) {
+//         console.log (num);
+//         num--;
+// }
+
+
+// let num = 5;
+// while (num) console.log (num--);
+
+
+// let num = 0;
+// do {
+//     console.log (num);
+//     num++
+// } while (num < 0);
+
+
+// for (let num = 0; num < 5; num++) {
+//     console.log (num);
+// }
+
+
+// let num = 0;
+// for (; num < 5; num++) {
+//     console.log(num);
+//         if (num == 2) {
+//             break;
+//         }
+// }
+
+
+// let num = 0;
+// for (; num < 5; num++) {
+//     if (num == 2) continue;
+//     console.log (num); 
+// }
+
+
+
+// firstFor: for (let num = 0; num < 2; num++) {
+//     for (let size = 0; size < 3; size++) {
+//         if (size == 2) {
+//             break firstFor;
+//         }
+//         console.log (size);
+//     }
+// }
+
+
+// firstFor: for (let num = 0; num < 2; num++) {
+//     for (let size = 0; size < 3; size++) {
+//         if (size == 2) {
+//             continue firstFor;
+//         }
+//         console.log (size);
+//     }
+// }
+
+
+// let num = 0;
+//     while (num < 5) {
+//         num ++;
+//             console.log (num);
+//     }
+
+
+
+// let num = 0;
+// do {
+//     console.log (num) 
+//         num++; 
+//     }   while (num < 5);
+//             console.log (num);
+
+
+
+// let num = 0;
+// do {
+//     console.log (num);
+//     num++;
+// } while (num < 5) {
+//     console.log(num);
+// }
+
+
+// for (let num = 0; num <= 5; num++) {
+//     console.log (num);
+// }
+
+
+
+// let num = 8;
+// while (num) {
+//     console.log (num);
+//     num--;
+// }
+
+
+// for (let num = 0; num < 3; num ++){
+//     console.log (`Число: ${num}`);
+// }
+
+
+// let num = 0;
+// while (num < 3){
+//     console.log(`Число: ${num}`);
+//     num++;
+// }
+
+
+CycleNew: for (let num = 0; num < 2; num++) {
+    for (let size = 0; size < 3; size++) {
+        if (size == 1) {
+            break CycleNew;
+        }
+        console.log (size);
+    }
+}
