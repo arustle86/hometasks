6170ebe804cedb097d0f67dcd0479660e958a5d1
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-08-25 18:56:41 +1000

    Ls6

---
+++ Study/FL/Ls6/Js/script.js
@@ -0,0 +1,67 @@
+// let massage = `Hi, freelanser!`
+// if (2 > 0) {
+//     console.log(massage);
+// }
+
+
+// let massage = `Hi, freelanser!`
+// if (2 + 1 === 3 && `1` == 1 || 10 > 5 && 10 === 1) {
+//     console.log(massage);
+// }
+
+
+
+
+// let massage = `Hi, freelanser!`
+// let number = 115;
+
+// if (number > 50) {
+//     console.log(`number > 50`)
+// } else if (number > 30) {
+//     console.log(`number > 30`)
+// } else if ( number > 10) { 
+//     console.log(`number > 10`)
+// } else {
+//     console.log(`Загадано маленькое число`)
+// }
+
+
+
+// let massage = `Hi`;
+// let massageEnd = (5 > 15) ? ` , Vasya` : ` , Olya`;
+// massage += massageEnd;
+// console.log(massage);
+
+
+
+// let massage = `Hi`;
+// let massageEnd = (5 > 15) ? ` , Vasya` :
+//                     (5 > 20) ? ` , Olya` :
+//                         (5 > 30) ? ` , Katya`: ` , Noname`;
+// massage += massageEnd;
+// console.log(massage);
+
+// if (1 === `1`) {
+//     console.log(`true!!!`);
+// } else {
+//     console.log(`false!!!`);
+// }
+
+
+// if (5 == `5`) {
+//     console.log(`true!!!`);
+// } else {
+//     console.log(`false!!!`);
+// }
+
+
+// let massage = (92 > `11` && 58 < 100) ? `true!!!` : `false!!!`;
+// console.log(massage);
+
+
+
+if (0) {
+    console.log(`false!!!!`)
+} else if (` `) {
+    console.log(`true!!!`);
+}
