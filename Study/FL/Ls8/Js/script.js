c615f593cf1b43e7f2b3bc62c6fb5edafb2c70cf
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-08-29 20:23:01 +1000

    Ls8

--- Study/FL/Ls8/Js/script.js
+++ Study/FL/Ls8/Js/script.js
@@ -4,6 +4,7 @@
 // showMassage();
 
 
+
 // function getSum() {
 //     let numOne, numThow;
 // }
@@ -20,6 +21,7 @@
 // getSum();
 
 
+
 // let massage = `Msg 1`;
 // function showMassage() {
 //     let massage = `Msg 2`;
@@ -27,6 +29,7 @@
 // }
 // console.log(massage);
 // showMassage();
+
 
 
 // let globalVar = `Я глобальная переменная`;
@@ -49,6 +52,7 @@
 // getSum();
 
 
+
 // function calcSum(numOne, numThu) {
 //     console.log(`Firs Number: ${numOne}`);
 //     console.log(`Second Number: ${numThu}`);
@@ -58,18 +62,150 @@
 // calcSum(1, 2);
 
 
-function calcSum(numOne, numThu, more, less) {
-    let numSum = numOne + numThu;
-    if(numSum > 3) {
-        more();
-    } else {
-        less();
-    }
-}
-    function showMoreMassage() {
-    console.log(`More than 3`);
-}
-function showLesMassage() {
-    console.log(`Min than 3`);
-}
-calcSum(1, 1, showMoreMassage, showLesMassage);
+
+// function calcSum(numOne, numThu, more, less) {
+//     let numSum = numOne + numThu;
+//     if(numSum > 3) {
+//         more();
+//     } else {
+//         less();
+//     }
+// }
+//     function showMoreMassage() {
+//     console.log(`More than 3`);
+// }
+// function showLesMassage() {
+//     console.log(`Min than 3`);
+// }
+// calcSum(1, 1, showMoreMassage, showLesMassage);
+
+
+
+// function calcSum(numOne, numThu) {
+//     let numSum = numOne + numThu;
+//     return numSum;
+// }
+// let funcRezult = calcSum (1, 2);
+// console.log(`Price: ${funcRezult}`);
+
+
+
+// function getSum(numOne, numThu) {
+//  let numSum = calcSum(numOne, numThu);
+//  console.log(numSum);
+// }
+// function calcSum(numOne, numThu) {
+//     return numOne + numThu;
+// }
+// getSum (3, 7);
+
+
+
+// function calcSum(numOne, numThu) {
+//     let result = 1;
+// for (let i = 0; i < numThu; i++) {
+//     result *= numOne;
+//         }
+//     return result;
+// }
+// console.log(calcSum(2, 3));
+
+
+
+// function calcSum(numOne, numThu) {
+//     if(numThu === 1) {
+//         return numOne;
+//       } else {
+//         return numOne * calcSum(numOne, numThu  - 1);
+//     }
+// }
+// console.log(calcSum(2, 3));
+
+
+
+// let showMassage = function() {
+//     console.log(`Hi`);
+// }
+// showMassage();
+
+
+
+// function getSum() {
+//     let summ = 1 + 2;
+//     console.log(summ);
+// }
+// let sumVar = getSum;
+// sumVar();
+// getSum();
+
+
+
+// `use strict`
+// if(2 > 1) {
+//     function getSum() {
+//         let sum = 2 + 1;
+//         console.log(sum);
+//     }
+// }
+// getSum();
+
+
+
+// let getMassage = (text , name) => text + `, `+ name;
+// console.log(getMassage(`Hi`, `Vasya`));
+
+
+
+// let getMassage = (text, name) => {
+//     let massage = text + `, ` + name + ` !`;
+//     return massage;
+// }; 
+// console.log(getMassage(`Hi`, `Vasya`));
+
+
+
+// function showMassage(text, name) {
+//     console.log(`${text}, ${name}!`);
+// }
+// setTimeout(showMassage, 3000, `Hi `, `Vasya!`)
+
+
+
+// function showMassage(text, name) {
+//     console.log(`${text}, ${name}!`);
+//     setTimeout(showMassage, 500, text, name)
+// }
+// setTimeout(showMassage, 500, `Hi `, `Vasya!`)
+
+
+
+// function showNumber(num) {
+//     console.log(num);
+//     if (num < 5) {
+//     setTimeout(showNumber, 3000, ++num);
+//     }
+// }
+// setTimeout(showNumber, 3000, 1);
+
+
+
+// function showNumber(num) {
+//     console.log(num);
+//     let timeId = setTimeout(showNumber, 1000, ++num);
+//     if (num === 6) {
+//         clearTimeout(timeId);
+//     }
+// }
+
+// setTimeout(showNumber, 1000, 1);
+
+
+// let result = 0;
+// function showNumber(num) {
+//     result += num;
+//     console.log(result);
+//     if (result ===5){
+//         clearInterval(timeId);
+//     }
+// }
+// let timeId = setInterval(showNumber, 1000, 1);
