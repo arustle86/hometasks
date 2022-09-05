116687d01a088a8385699691ad5149be09299b07
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-05 16:18:45 +1000

    Update array.js
    push

--- Study/SB/array.js
+++ Study/SB/array.js
@@ -1,16 +1,16 @@
-let alphabet = [];
+let fruits = [`apple`, `banan`, `mango`, `orange`];
 
-alphabet.push (`Г`);
-alphabet.push (`Д`);
-alphabet.push (`Е`, `Ж`, `З`);
-alphabet.unshift (`В`);
-alphabet.unshift (`А`, `Б`);
-console.log (alphabet);
-alphabet.length;
-alphabet[alphabet.length - 2];
+let alhpabet = [];
 
+alhpabet.push(`Г`);
+alhpabet.push(`Д`);
+alhpabet.push(`Е`, `Ж`,`З`);
 
-let greetings = [`hello`, `hi`, `good evning`, `good afternun`];
-console.log (greetings[2]);
-greetings[2] = `good evening`;
-console.log (greetings[2]);
+alhpabet.unshift(`В`);
+alhpabet.unshift(`А`,`Б`);
+
+let greetings = [`hello`, `hi`,`good eavning`, `how are you`];
+console.log(greetings[2]);
+
+greetings[2] = `good evenings`;
+console.log(greetings[2]);
