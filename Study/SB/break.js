cec5767feb673ea4885862d14cc43ee02c2f940a
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-06 12:34:06 +1000

    commit

--- Study/SB/break.js
+++ Study/SB/break.js
@@ -1,13 +1,13 @@
-let cards = ['2', 'Туз', 'Король', '10', 'Дама', '7','Туз'];
-console.log('Ищем даму в колоде...');
+let cards = [`2`, `Туз`, `Король`, `10`, `Дама`, `7`,`Туз`];
+console.log(`Ищем даму в колоде...`);
 
 let found = false;
 
 for (let card of cards) {
-    console.log('Из колоды вытянута карта ${card}');
-    if (card === 'Дама') {
+    console.log(`Из колоды вытянута карта ${card}`);
+    if (card === `Дама`) {
         found = true;
         break;
     }
 }
-console.log(found ? 'Мы нашли даму' : 'В этой колоде дамы нет');
+console.log(found ? `Мы нашли даму` : `В этой колоде дамы нет`);
