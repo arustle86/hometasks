87b35e7f46ecfe248df869e5c5999084738cf23d
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-12 17:26:45 +1000

    dom

--- Study/SB/dom/script.js
+++ Study/SB/dom/script.js
@@ -1,3 +1,10 @@
-let text = document.querySelector(`.text`).style.color=`blue`;
+// let text = document.querySelectorAll (`.text`).style.color=`pink`;
 
-text.style.color = `blue`;
+// text.style.color = `green`;
+
+document.querySelectorAll(`.text`).forEach(function(element) {
+    element.style.color = `pink`;
+})
+
+let block = document.querySelector(`.block`);
+block.classList.add(`translate`);
