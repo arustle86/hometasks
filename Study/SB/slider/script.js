2deeb5c86b0d4bab2d165dbbd1a61d8284bbfae4
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-13 17:40:43 +1000

    slider

--- Study/SB/slider/script.js
+++ Study/SB/slider/script.js
@@ -1,21 +1,15 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+const swiper = new Swiper ('.swiper', {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    loop: true,
+    spaceBetween: 30,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
 });
