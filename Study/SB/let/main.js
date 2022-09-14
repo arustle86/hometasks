a68a9f774400c56df3fac7bcb493784e140e9ccf
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-14 14:02:17 +1000

    beging again

---
+++ Study/SB/let/main.js
@@ -0,0 +1,113 @@
+// let x = 42;
+
+// let digit = 25;
+
+// let fract = 25.03;
+
+// let exp = 2e10;
+
+// let expSmall = 2e-3;
+
+// let expFract = 1.25e3;
+
+// let expFractSmall = 1.25e-3;
+
+// let fract1 = 25.;
+
+// let fract2 = .25;
+
+// let fract3 = -25.;
+
+// let fract4 = -.25;
+
+// // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+// // 0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001
+
+// let dec = 9;
+
+// let bin = 0b1001;
+
+// // 8
+
+// let oct = 0o10;
+
+// //16 
+
+// let y = 0x1b;
+// let Y = 0x1B;
+
+// // rgb(255, 255, 255)
+// // #fff
+
+// let z = 3 + 10; // 13
+
+// let c = 13 - 3; // 10
+
+// let v = 10 * 3; // 30
+
+// let b = 10 / 3; // 3.333333
+
+// let n = 10 % 3; // 1
+
+// let complex = 10 + 5 * 10 - 8 / 2; // 56
+// let complex2 = (10 + 5) * (10 - 8) / 2; // 15
+
+// let a = 10;
+// a++; // 11
+// a--; // 10
+// ++a; // 11
+// --a; // 10
+
+
+// console.log(a++); // 10
+// console.log(a); // 11
+
+// console.log(a--); // 11
+// console.log(--a); // 10
+
+
+
+
+// x += 5; // x = x + 5
+// x -= 5; // x = x - 5
+
+// x *= 5; // x = x * 5
+// x /= 5; // x = x / 5
+
+// x %= 5; // x = x % 5
+
+// // задача 1 //
+
+// let entrance = 4; // количество подъездов
+
+// let floors = 9; // количество этажей
+
+// let flatsPerFloors = 4; // количество квартир на этаже
+
+// let flatsPerEntrance = floors * flatsPerFloors;
+// console.log(`Количество квартир в подъезде: ` + flatsPerEntrance);
+
+// let flatsPerHome = flatsPerEntrance * entrance;
+// console.log(`Количество квартир в доме: ` + flatsPerHome);
+
+// // задача 2 //
+
+// let vodka = 50;
+// let tomatoJuice = 120;
+// let lemonJuice = 10;
+// let tabasco = 1;
+// let worcester = 1;
+
+// let volume = 500; 
+
+// let k = (vodka + tomatoJuice + lemonJuice + tabasco + worcester) / volume;
+// console.log(vodka * k);
+
+let a = 3;
+let b = 5;
+let c = 10;
+
+let d = b * b - 4 * a * c;
+
+let dRoot = Math.sqrt(i);
