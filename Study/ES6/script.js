78ef657d53ed9a6162f3e8546c69a6f3c6e4670b
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-14 19:39:29 +1000

    Study

---
+++ Study/ES6/script.js
@@ -0,0 +1,86 @@
+"use strict"
+
+// let user = "Alex";
+//     age = 25;
+//     email = "1@may.ru";
+
+// let cl = "class = \"my class\"";
+// let cl = 'class = "my class"';
+// console.log(cl);
+
+// let isWin = true, isCheckedField = false;
+// let isGreater = 4 < 1;
+// console.log(isGreater);
+
+// let age = prompt(`Сколько Вам лет?`, 18);
+// console.log(age);
+
+// let isCar = confirm("У тебя есть машина?");
+// console.log(isCar);
+
+// let x = 0, sgn = 0;
+//     if (x < 0) {
+//         sgn = -1;
+//         console.log(`x отрицательное число, `, sgn);
+//     } else {
+//         if (x > 0) {
+//             sgn = 1;
+//             console.log(`x положительное число, `, sgn);
+//         }
+//         else {
+//             console.log(`x равен 0, `, sgn);
+//         }
+//     } 
+
+// let age = 25;
+// let currentAge = age > 18;
+// console.log(currentAge);
+    
+
+// let x = 41;
+//     if (x > 2 && x < 7) {
+//         console.log(`x попадает в нужный диапазон`);
+//     } else {
+//         console.log(`x НЕ попдходит`);
+//     }
+
+// x в диапозоне [2 ; 7]; y не пренадлежит [0 ; 5]
+// let x = 4, y = -2;
+// if (x >= 2 && x <= 7 && (y < 0 || y > 5)) {
+//     console.log(`x находится в диапазоне [2 ; 7], а y не находится в диапазоне [0 ; 5]`);
+// } else {
+//     console.log( `Данные не подходят по условию выражения`);
+// }
+
+// let item = 3;
+// switch (item) {
+//     case 1: console.log(`no`);
+//     case 2: console.log(`no`);
+//     case 3: console.log(`yes`); break;
+//     case 4: console.log(`no`);
+//     case 5: console.log(`no`);
+//     default: console.log(`another`);
+// } 
+
+
+// let s = 0, i = 1;
+//     while (i <= 1000) {
+//         s += 1 / i;
+//         ++i;
+//     }
+//         console.log(s);
+
+// let s = 0, i = 1;
+//     while (i <= 1000 && s < 5) {
+//         s += 1/i;
+//         ++i;
+//     } console.log(s);
+
+// let s = 0, i = 1;
+//     while ((s += i++) < 100);
+//     console.log(s);
+
+// let s = 0;
+//     for (let i = 1; i <= 1000; ++i) {
+//         s += 1 / i;
+//     } console.log(s);
