bc95cb5c5739c5b6f9dfead50787519e43968479
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-17 13:42:43 +1000

    php

--- Мой/spruce-html/mail.php
+++ Мой/spruce-html/mail.php
@@ -17,34 +17,34 @@
 
         // Set the recipient email address.
         // Note:  Update this to your desired email address.
-        $recipient = "modinatheme@gmail.com";
+        $recipient = "a.tsurkanu@gmail.com";
 
         // Set the email subject.
-        $subjectname = "New Contact from $name";
+        $subjectname = "Новое сообщение $name";
 
         // Build the email content.
-        $email_content = "Name: $name  \r\n\n";
-        $email_content .= "Email: $email \r\n\n";
-        $email_content .= "Message: $message \r\n\n";
+        $email_content = "Имя: $name  \r\n\n";
+        $email_content .= "Адрес: $email \r\n\n";
+        $email_content .= "Сообщение: $message \r\n\n";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "От: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Спасибо! Ваше сообщение отправлено";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Упс! Что - то пошло не так";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Ошибка, попробуйте еще раз";
     }
 
 ?>
