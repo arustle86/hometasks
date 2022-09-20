84a5c73fdcaeef8a781281c39ee00a8f48875aa5
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-20 13:54:57 +1000

    push

--- Study/SB/typescript/index.ts
+++ Study/SB/typescript/index.ts
@@ -1,10 +1,4 @@
-
-import { sum } from "./math/sum";
-import { sub } from "./math/sub";
-import { mul } from "./math/mul";
-import { div } from "./math/div";
-
-sum(1, 2); //?
-sub(1, 2); //?
-mul(1, 2); //?
-div(1, 2); //? 
+export * from "./sum";
+export * from "./sub";
+export * from "./mul";
+export * from "./div";
