bad9d09eb61de6b36c1c9c8eff3e462567763d97
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-09-27 19:31:57 +1000

    push

--- Study/ES6/script.js
+++ Study/ES6/script.js
@@ -157,18 +157,17 @@
 // let res = div(8, 0);
 // console.log(res);
 
-function showPrimes(n) {
-  for (let i = 2; i < n; i++) {
-    if (!isPrime(i)) continue;
+// function showPrimes(n) {
+//   for (let i = 2; i < n; i++) {
+//     if (!isPrime(i)) continue;
 
-    console.log(i);
-  }
-}
-function isPrime(n) {
-  for (let i = 2; i < n; i++) {
-    if (n % i == 0) return false;
-  }
-  return true;
-}
-
-showPrimes(10);
+//     console.log(i);
+//   }
+// }
+// function isPrime(n) {
+//   for (let i = 2; i < n; i++) {
+//     if (n % i == 0) return false;
+//   }
+//   return true;
+// }
+// showPrimes(10);
