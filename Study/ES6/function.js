18f7b4a4ed97b663d055e9a50c2419b385295a27
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-18 18:27:37 +1000

    turnout function

--- Study/ES6/function.js
+++ Study/ES6/function.js
@@ -1,5 +1,8 @@
 "use strict";
 
-setTimeout (function(){
-    console.log("Анонимная функция");
-}, 5000);
+// let anonym = function () {
+//     console.log("Это анонимная функция");
+// }
+
+let anonym = (a, b) => a + b;
+    console.log(anonym(15, 4));
