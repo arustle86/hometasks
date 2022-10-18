59f4bc9f7e6ffa8a3d57fcaf5cbf4ccf2d90f051
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-18 17:40:27 +1000

    timeOutFunc

--- Study/ES6/function.js
+++ Study/ES6/function.js
@@ -1,13 +1,5 @@
-"use strict"
+"use strict";
 
-let age = prompt("Сколько Вам лет?", 18);
- 
-    let setAcces = (age < 18) ?
-
-        function() {
-            console.log("Вы слишком молоды");
-        } : function() {
-            console.log("Всё ок");
-        }
-
-setAcces();
+setTimeout (function(){
+    console.log("Анонимная функция");
+}, 5000);
