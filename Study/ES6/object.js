4e76e638b785b03114e256c31337c17c8e0b5d48
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-19 16:28:20 +1000

    commit

--- Study/ES6/object.js
+++ Study/ES6/object.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let book = {
-    title: "название",
-    author: "автор",
-    nPages: 0,
-    price: 0,
-    "size book": {height: 100, widht: 20}
-};
-    let keyName = prompt("Что Вы хотите узнать о книге?", "");
-    console.log(book[keyName]);
+let newKay = "color";
+
+    let car = {
+        model: "Toyota",
+        [newKay]: "black",
+    };
+
+        console.log(car[newKay]);
+        console.log(car.color);
