bc12b2d4a7a333f1b4cdf5539ef0d86e130f40bd
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-19 16:05:13 +1000

    commit

--- Study/ES6/object.js
+++ Study/ES6/object.js
@@ -1,8 +1,11 @@
+"use strict";
+
 let book = {
     title: "название",
     author: "автор",
     nPages: 0,
-    price: 0
+    price: 0,
+    "size book": {height: 100, widht: 20}
 };
     book.isSalled = false;
     book.isSalled = true;
@@ -14,3 +17,10 @@ let book = {
     console.log(book.nPages);
     console.log(book);
     console.log("nPages" in book);
+
+    if (book.nPages === undefined) {
+        console.log("nPages не существует");
+    }
+    book["size book"] = {height: 90, widht: 55};
+    delete book["size book"]
+    console.log(book["size book"]);
