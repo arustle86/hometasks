77f10f74d5735795147e45ac9601ccf349c8c4d8
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-19 14:02:42 +1000

    object

---
+++ Study/ES6/object.js
@@ -0,0 +1,14 @@
+let book = {
+    title: "название",
+    author: "автор",
+    nPages: 0,
+    price: 0
+};
+    book.isSalled = false;
+    book.isSalled = true;
+
+    delete book.nPages;
+
+    console.log(book.title);
+    console.log(book.isSalled);
+    console.log(book);
