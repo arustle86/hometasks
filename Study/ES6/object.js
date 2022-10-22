14e19597193fc087341f37c870f7626a8885abbf
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-22 17:37:41 +1000

    get

--- Study/ES6/object.js
+++ Study/ES6/object.js
@@ -1,24 +1,11 @@
 "use strict";
 
     let car = {
-        model: "toyota",
-        color: "black",
-        go(driverName) {
-            console.log("Водитель " + driverName + ": машина едет");
-        },
-        stop() {
-            console.log("Машина остановилась");
-        },
+        model: "Toyota",
         getModel(){
             return this.model;
-        },
-        getColor(){
-            return this.color;
         }
     };
 
-
-    car.go("Леонид");
-    car.stop();
+    let get = car.getModel();
     console.log(car.getModel());
-    console.log(car.getColor());
