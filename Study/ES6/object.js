83480f75d44ec5b5b018a8c08a3b36a0cf6f1c70
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-22 19:39:09 +1000

    clone

--- Study/ES6/object.js
+++ Study/ES6/object.js
@@ -1,11 +1,24 @@
 "use strict";
 
-    let car = {
-        model: "Toyota",
-        getModel(){
-            return this.model;
-        }
+    let book = {
+        title: "My-My",
+        author: "Тургенев",
+        nShits: 543,
+        price: 299,
+        size: {height: 100, widht: 50}
     };
 
-    let get = car.getModel();
-    console.log(car.getModel());
+    let jurnal = cloneObject({}, book);
+    book.size.height = 0;
+    console.log(jurnal);
+
+    function cloneObject(newObject, oldObjedt) {
+        for (let key in oldObjedt) {
+            if((typeof oldObjedt[key]) == "object") {
+                newObject[key] = cloneObject({}, oldObjedt[key]);
+            } else {
+                newObject[key] = oldObjedt[key];
+            }
+        }
+      return newObject;
+    }
