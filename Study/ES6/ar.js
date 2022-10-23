5d414e589a0b4c4abcbac07b44a60a2f78ce4724
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-23 16:27:38 +1000

    commit

--- Study/ES6/ar.js
+++ Study/ES6/ar.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let ar = [1, 2, 3, 5];
-    ar[1] = "apple";
-    console.log(ar[0], ar[1], ar[2], ar[3]);
-    console.log(ar);
+let ar = ["Яблоко", {name : "John"}, 
+            true, 
+            function() {alert("Привет");}];
+
+console.log(ar[1]);
