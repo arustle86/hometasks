ff95f27e9680231636b48bb68e534c9227333e79
Александр Цуркану <108608175+arustle86@users.noreply.github.com>
2022-10-23 20:15:02 +1000

    reverse array

--- Study/ES6/ar.js
+++ Study/ES6/ar.js
@@ -1,17 +1,29 @@
 "use strict";
 
-    let matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ];
+ let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+ let b = ["a", "b", "c", "d", "e", "f"];
+    
+    // console.log(a.reverse());
 
-    matrix [1][1] = 0;
+    // function rev1 (a) {
+    //     return a.reverse();
+    // }
+    //      console.log(rev1(a));
 
-        for (let row of matrix) {
-            let cols = "";
-                for (let val of row) {
-                    cols += val + " ";
-                }
-                console.log(cols);
-        }
+    // function rev2 (a) {
+    //     res = [];
+    //     a.forEach(element => {
+    //         res.unshift(element)
+    //     });
+    //     return res;
+    // }
+    //     console.log(rev2(b));
+
+    function rev3 (a) {
+        res = [];
+            for (let i = a.lenght - 1; i >= 0; i--) {
+                res.push(a[i]);
+            }
+        return res;    
+    }
+        console.log(rev3(b));
